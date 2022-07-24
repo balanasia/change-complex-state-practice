f43362922689e571daab66ac80e85698e8a4417c
Anastasiia Balakhan <41988936+balanasia@users.noreply.github.com>
2022-07-24 14:09:53 -0500

    Solve the challenge passing in the values as an array and passing in the previous value of the variable to keep the state going

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,47 @@
 import React, { useState } from "react";
 
+//CHALLENGE: Make the code in App.jsx work.
+//The final app should have a single contact
+//with fName, lName and email.
+
+//HINT: You'll need to apply the following things you learnt:
+//1. Using JS Objects with state.
+//2. Making use of previous state when changing state.
+//3. Working with forms in React.
+//4. Handing events
+
 function App() {
   const [contact, setContact] = useState({
     fName: "",
     lName: "",
     email: ""
   });
+
+  function handleChange(event) {
+    const { value, name } = event.target;
+
+    setContact((prevValue) => {
+      if (name === "fName") {
+        return {
+          fName: value,
+          lName: prevValue.lName,
+          email: prevValue.email
+        };
+      } else if (name === "lName") {
+        return {
+          fName: prevValue.fName,
+          lName: value,
+          email: prevValue.email
+        };
+      } else if (name === "email") {
+        return {
+          fName: prevValue.fName,
+          llName: prevValue.lName,
+          email: value
+        };
+      }
+    });
+  }
 
   return (
     <div className="container">
@@ -14,9 +50,9 @@ function App() {
       </h1>
       <p>{contact.email}</p>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
-        <input name="email" placeholder="Email" />
+        <input onChange={handleChange} name="fName" placeholder="First Name" />
+        <input onChange={handleChange} name="lName" placeholder="Last Name" />
+        <input onChange={handleChange} name="email" placeholder="Email" />
         <button>Submit</button>
       </form>
     </div>
